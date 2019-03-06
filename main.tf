9d84a89883fbc00f9be4f67725ba63946bae570b
Jake Lundberg <grove.mountain.github@nym.hush.com>
2019-03-05 16:44:41 -0800

    jlundberg-add-service-principal: adding in service principals and outputs to make variable instantiation easier

--- main.tf
+++ main.tf
@@ -1,273 +1,275 @@
-##############################################################################
-# HashiCorp Terraform and Vault Workshop
-# 
-# This Terraform configuration will create the following:
-#
-# Azure Resource group with a virtual network and subnet
-# A Linux server running HashiCorp Vault and a simple application
-# A hosted Azure MySQL database server
+provider "azurerm" {
+  version = "~> 1.22"
+}
 
-/* First we'll create a resource group. In Azure every resource belongs to a 
-resource group. Think of it as a container to hold all your resources. 
-You can find a complete list of Azure resources supported by Terraform here:
-https://www.terraform.io/docs/providers/azurerm/. Note the use of variables 
-to dynamically set our name and location. Variables are usually defined in 
-the variables.tf file, and you can override the defaults in your 
-own terraform.tfvars file. */
+resource "random_id" "project_name" {
+  byte_length = 4
+}
+
+resource "random_id" "client_secret" {
+  byte_length = 32
+}
+
+resource "random_id" "vault_token" {
+  byte_length = 32
+}
 
 resource "azurerm_resource_group" "vaultworkshop" {
   name     = "${var.prefix}-vault-workshop"
   location = "${var.location}"
 }
 
-/* The next resource is a Virtual Network. We can dynamically place it into the
-resource group without knowing its name ahead of time. Terraform handles all
-of that for you, so everything is named consistently every time. Say goodbye
-to weirdly-named mystery resources in your Azure Portal. To see how all this
-works visually, run `terraform graph` and copy the output into the online
-GraphViz tool: http://www.webgraphviz.com/ */
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  location            = "${azurerm_resource_group.vaultworkshop.location}"
+  address_space       = ["${var.address_space}"]
+  resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
+}
 
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "${var.prefix}-vnet"
-#   location            = "${azurerm_resource_group.vaultworkshop.location}"
-#   address_space       = ["${var.address_space}"]
-#   resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
-# }
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name  = "${azurerm_resource_group.vaultworkshop.name}"
+  address_prefix       = "${var.subnet_prefix}"
+}
 
-/* Next we'll build a subnet to run our VMs in. These variables can be defined 
-via environment variables, a config file, or command line flags. Default 
-values will be used if the user does not override them. You can find all the
-default variables in the variables.tf file. You can customize this demo by
-making a copy of the terraform.tfvars.example file. */
+resource "azurerm_network_security_group" "vault-sg" {
+  name                = "${var.prefix}-sg"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
 
-# resource "azurerm_subnet" "subnet" {
-#   name                 = "${var.prefix}-subnet"
-#   virtual_network_name = "${azurerm_virtual_network.vnet.name}"
-#   resource_group_name  = "${azurerm_resource_group.vaultworkshop.name}"
-#   address_prefix       = "${var.subnet_prefix}"
-# }
+  security_rule {
+    name                       = "Vault"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8200"
+    source_address_prefix      = "${var.vault_source_ips}"
+    destination_address_prefix = "*"
+  }
 
-/* Now that we have a network, we'll deploy a stand-alone HashiCorp Vault 
-server. Vault supports a 'dev' mode which is appropriate for demonstrations
-and development purposes. In other words, don't do this in production.
-An Azure Virtual Machine has several components. In this example we'll build
-a security group, a network interface, a public ip address, a storage 
-account and finally the VM itself. Terraform handles all the dependencies 
-automatically, and each resource is named with user-defined variables. */
+  security_rule {
+    name                       = "Transit-App"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-# resource "azurerm_network_security_group" "vault-sg" {
-#   name                = "${var.prefix}-sg"
-#   location            = "${var.location}"
-#   resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
 
-#   security_rule {
-#     name                       = "Vault"
-#     priority                   = 100
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "8200"
-#     source_address_prefix      = "${var.vault_source_ips}"
-#     destination_address_prefix = "*"
-#   }
+  security_rule {
+    name                       = "SSH"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "${var.ssh_source_ips}"
+    destination_address_prefix = "*"
+  }
+}
 
-#   security_rule {
-#     name                       = "Transit-App"
-#     priority                   = 102
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "5000"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
+resource "azurerm_network_interface" "vault-nic" {
+  name                      = "${var.prefix}-vault-nic"
+  location                  = "${var.location}"
+  resource_group_name       = "${azurerm_resource_group.vaultworkshop.name}"
+  network_security_group_id = "${azurerm_network_security_group.vault-sg.id}"
 
-#   security_rule {
-#     name                       = "SSH"
-#     priority                   = 101
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "22"
-#     source_address_prefix      = "${var.ssh_source_ips}"
-#     destination_address_prefix = "*"
-#   }
-# }
+  ip_configuration {
+    name                          = "${var.prefix}ipconfig"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.vault-pip.id}"
+  }
+}
 
-/* A network interface. This is required by the azurerm_virtual_machine 
-resource. Terraform will let you know if you're missing a dependency. */
+resource "azurerm_public_ip" "vault-pip" {
+  name                = "${var.prefix}-ip"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
+  allocation_method   = "Dynamic"
+  domain_name_label   = "${var.prefix}"
+}
 
-# resource "azurerm_network_interface" "vault-nic" {
-#   name                      = "${var.prefix}-vault-nic"
-#   location                  = "${var.location}"
-#   resource_group_name       = "${azurerm_resource_group.vaultworkshop.name}"
-#   network_security_group_id = "${azurerm_network_security_group.vault-sg.id}"
+resource "azurerm_virtual_machine" "vault" {
+  name                = "${var.prefix}-vault"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
+  vm_size             = "${var.vm_size}"
 
-#   ip_configuration {
-#     name                          = "${var.prefix}ipconfig"
-#     subnet_id                     = "${azurerm_subnet.subnet.id}"
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = "${azurerm_public_ip.vault-pip.id}"
-#   }
-# }
+  network_interface_ids         = ["${azurerm_network_interface.vault-nic.id}"]
+  delete_os_disk_on_termination = "true"
 
-/* Every Azure Virtual Machine comes with a private IP address. You can also 
-optionally add a public IP address for Internet-facing applications and 
-demo environments like this one. */
+  identity = {
+    type = "SystemAssigned"
+  }
 
-# resource "azurerm_public_ip" "vault-pip" {
-#   name                = "${var.prefix}-ip"
-#   location            = "${var.location}"
-#   resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
-#   allocation_method   = "Dynamic"
-#   domain_name_label   = "${var.prefix}"
-# }
+  storage_image_reference {
+    publisher = "${var.image_publisher}"
+    offer     = "${var.image_offer}"
+    sku       = "${var.image_sku}"
+    version   = "${var.image_version}"
+  }
 
-/* And finally we build our Vault server. This is a standard Ubuntu instance.
-We use the shell provisioner to run a Bash script that configures Vault for 
-the demo environment. Terraform supports several different types of 
-provisioners including Bash, Powershell and Chef. */
+  storage_os_disk {
+    name              = "${var.prefix}-osdisk"
+    managed_disk_type = "Standard_LRS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+  }
 
-# resource "azurerm_virtual_machine" "vault" {
-#   name                = "${var.prefix}-vault"
-#   location            = "${var.location}"
-#   resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
-#   vm_size             = "${var.vm_size}"
+  os_profile {
+    computer_name  = "${var.prefix}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
 
-#   network_interface_ids         = ["${azurerm_network_interface.vault-nic.id}"]
-#   delete_os_disk_on_termination = "true"
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 
-#   storage_image_reference {
-#     publisher = "${var.image_publisher}"
-#     offer     = "${var.image_offer}"
-#     sku       = "${var.image_sku}"
-#     version   = "${var.image_version}"
-#   }
+  provisioner "file" {
+    source      = "files/setup.sh"
+    destination = "/home/${var.admin_username}/setup.sh"
 
-#   storage_os_disk {
-#     name              = "${var.prefix}-osdisk"
-#     managed_disk_type = "Standard_LRS"
-#     caching           = "ReadWrite"
-#     create_option     = "FromImage"
-#   }
+    connection {
+      type     = "ssh"
+      user     = "${var.admin_username}"
+      password = "${var.admin_password}"
+      host     = "${azurerm_public_ip.vault-pip.fqdn}"
+    }
+  }
 
-#   os_profile {
-#     computer_name  = "${var.prefix}"
-#     admin_username = "${var.admin_username}"
-#     admin_password = "${var.admin_password}"
-#   }
+  provisioner "file" {
+    source      = "files/vault_setup.sh"
+    destination = "/home/${var.admin_username}/vault_setup.sh"
 
-#   os_profile_linux_config {
-#     disable_password_authentication = false
-#   }
+    connection {
+      type     = "ssh"
+      user     = "${var.admin_username}"
+      password = "${var.admin_password}"
+      host     = "${azurerm_public_ip.vault-pip.fqdn}"
+    }
+  }
 
-#   provisioner "file" {
-#     source      = "files/setup.sh"
-#     destination = "/home/${var.admin_username}/setup.sh"
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/${var.admin_username}/*.sh",
+      "sleep 30",
+      "MYSQL_HOST=${var.prefix}-mysql-server /home/${var.admin_username}/setup.sh"
+    ]
 
-#     connection {
-#       type     = "ssh"
-#       user     = "${var.admin_username}"
-#       password = "${var.admin_password}"
-#       host     = "${azurerm_public_ip.vault-pip.fqdn}"
-#     }
-#   }
+    connection {
+      type     = "ssh"
+      user     = "${var.admin_username}"
+      password = "${var.admin_password}"
+      host     = "${azurerm_public_ip.vault-pip.fqdn}"
+    }
+  }
+}
 
-#   provisioner "file" {
-#     source      = "files/vault_setup.sh"
-#     destination = "/home/${var.admin_username}/vault_setup.sh"
+resource "azurerm_mysql_server" "mysql" {
+  name                = "${var.prefix}-mysql-server"
+  location            = "${azurerm_resource_group.vaultworkshop.location}"
+  resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
+  ssl_enforcement     = "Disabled"
 
-#     connection {
-#       type     = "ssh"
-#       user     = "${var.admin_username}"
-#       password = "${var.admin_password}"
-#       host     = "${azurerm_public_ip.vault-pip.fqdn}"
-#     }
-#   }
+  sku {
+    name     = "B_Gen5_2"
+    capacity = 2
+    tier     = "Basic"
+    family   = "Gen5"
+  }
 
-#   provisioner "remote-exec" {
-#     inline = [
-#       "chmod +x /home/${var.admin_username}/*.sh",
-#       "sleep 30",
-#       "MYSQL_HOST=${var.prefix}-mysql-server /home/${var.admin_username}/setup.sh"
-#     ]
+  storage_profile {
+    storage_mb            = 5120
+    backup_retention_days = 7
+    geo_redundant_backup  = "Disabled"
+  }
 
-#     connection {
-#       type     = "ssh"
-#       user     = "${var.admin_username}"
-#       password = "${var.admin_password}"
-#       host     = "${azurerm_public_ip.vault-pip.fqdn}"
-#     }
-#   }
-# }
+  administrator_login          = "${var.admin_username}"
+  administrator_login_password = "${var.admin_password}"
+  version                      = "5.7"
+  ssl_enforcement              = "Disabled"
+}
 
-/* Azure MySQL Database
-Vault will manage this database with the database secrets engine.
-Terraform can build any type of infrastructure, not just virtual machines. 
-Azure offers managed MySQL database servers and a whole host of other 
-resources. Each resource is documented with all the available settings:
-https://www.terraform.io/docs/providers/azurerm/r/mysql_server.html */
+resource "azurerm_mysql_database" "wsmysqldatabase" {
+  name                = "wsmysqldatabase"
+  resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
+  server_name         = "${azurerm_mysql_server.mysql.name}"
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
 
-# resource "azurerm_mysql_server" "mysql" {
-#   name                = "${var.prefix}-mysql-server"
-#   location            = "${azurerm_resource_group.vaultworkshop.location}"
-#   resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
-#   ssl_enforcement     = "Disabled"
+data "azurerm_public_ip" "vault-pip" {
+  name                = "${azurerm_public_ip.vault-pip.name}"
+  depends_on          = ["azurerm_virtual_machine.vault"]
+  resource_group_name = "${azurerm_virtual_machine.vault.resource_group_name}"
+}
 
-#   sku {
-#     name     = "B_Gen5_2"
-#     capacity = 2
-#     tier     = "Basic"
-#     family   = "Gen5"
-#   }
+resource "azurerm_mysql_firewall_rule" "vault-mysql" {
+  name                = "vault-mysql"
+  resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
+  server_name         = "${azurerm_mysql_server.mysql.name}"
+  start_ip_address    = "${data.azurerm_public_ip.vault-pip.ip_address}"
+  end_ip_address      = "${data.azurerm_public_ip.vault-pip.ip_address}"
+}
 
-#   storage_profile {
-#     storage_mb            = 5120
-#     backup_retention_days = 7
-#     geo_redundant_backup  = "Disabled"
-#   }
 
-#   administrator_login          = "${var.admin_username}"
-#   administrator_login_password = "${var.admin_password}"
-#   version                      = "5.7"
-#   ssl_enforcement              = "Disabled"
-# }
+resource "azurerm_azuread_application" "vaultapp" {
+  name = "${var.prefix}-${random_id.project_name.hex}-vaultapp"
+}
 
-/* This is a sample database that we'll populate with data from our app.
-With Terraform, everything is Infrastructure as Code. No more manual steps,
-aging runbooks, tribal knowledge or outdated wiki instructions. Terraform
-is your executable documentation, and it will build infrastructure correctly
-every time. */
+resource "azurerm_azuread_service_principal" "vaultapp" {
+  application_id = "${azurerm_azuread_application.vaultapp.application_id}"
+}
 
-# resource "azurerm_mysql_database" "wsmysqldatabase" {
-#   name                = "wsmysqldatabase"
-#   resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
-#   server_name         = "${azurerm_mysql_server.mysql.name}"
-#   charset             = "utf8"
-#   collation           = "utf8_unicode_ci"
-# }
+resource "azurerm_azuread_service_principal_password" "vaultapp" {
+  service_principal_id = "${azurerm_azuread_service_principal.vaultapp.id}"
+  value                = "${random_id.client_secret.id}"
+  end_date             = "2020-01-01T01:02:03Z"
+  depends_on           = ["azurerm_role_assignment.role_assignment"]
+}
 
-/* Public IP addresses are not generated until they are attached to an object.
-So we use a 'data source' here to fetch it once its available. Then we can
-provide the public IP address to the next resource for allowing firewall 
-access to our database. */
+resource "azurerm_virtual_machine_extension" "virtual_machine_extension" {
+  name                 = "vault"
+  location             = "${var.location}"
+  resource_group_name  = "${azurerm_resource_group.vaultworkshop.name}"
+  virtual_machine_name = "${azurerm_virtual_machine.vault.name}"
+  publisher            = "Microsoft.ManagedIdentity"
+  type                 = "ManagedIdentityExtensionForLinux"
+  type_handler_version = "1.0"
 
-# data "azurerm_public_ip" "vault-pip" {
-#   name                = "${azurerm_public_ip.vault-pip.name}"
-#   depends_on          = ["azurerm_virtual_machine.vault"]
-#   resource_group_name = "${azurerm_virtual_machine.vault.resource_group_name}"
-# }
+  settings = <<SETTINGS
+    {
+        "port": 50342
+    }
+SETTINGS
+}
 
-/* Allows the Linux VM to connect to the MySQL database, using the IP address
-from the data source above. */
+data "azurerm_subscription" "subscription" {}
 
-# resource "azurerm_mysql_firewall_rule" "vault-mysql" {
-#   name                = "vault-mysql"
-#   resource_group_name = "${azurerm_resource_group.vaultworkshop.name}"
-#   server_name         = "${azurerm_mysql_server.mysql.name}"
-#   start_ip_address    = "${data.azurerm_public_ip.vault-pip.ip_address}"
-#   end_ip_address      = "${data.azurerm_public_ip.vault-pip.ip_address}"
-# }
+data "azurerm_client_config" "current" {}
+
+data "azurerm_builtin_role_definition" "builtin_role_definition" {
+  name = "Contributor"
+}
+
+# Grant the VM identity contributor rights to the current subscription
+resource "azurerm_role_assignment" "role_assignment" {
+  scope              = "${data.azurerm_subscription.subscription.id}"
+  role_definition_id = "${data.azurerm_subscription.subscription.id}${data.azurerm_builtin_role_definition.builtin_role_definition.id}"
+  principal_id       = "${azurerm_azuread_service_principal.vaultapp.id}"
+
+  lifecycle {
+    ignore_changes = ["name"]
+  }
+}
+
--- outputs.tf
+++ outputs.tf
@@ -8,21 +8,44 @@
 # providing instructions to the user on how to connect to their own custom 
 # demo environment.
 #
-# output "Vault_Server_URL" {
-#   value = "http://${azurerm_public_ip.vault-pip.fqdn}:8200"
-# }
-# output "MySQL_Server_FQDN" {
-#   value = "${azurerm_mysql_server.mysql.fqdn}"
-# }
-# output "_Instructions" {
-#   value = <<SHELLCOMMANDS
-#
-# # Connect to your Linux Virtual Machine
-# #
-# # Run the command below to SSH into your server. You can also use PuTTY or any
-# # other SSH client. Your password is: ${var.admin_password}
-# 
-# ssh ${var.admin_username}@${azurerm_public_ip.vault-pip.fqdn}
-# SHELLCOMMANDS
-# }
+
+output "azure_tenant_id" {
+    value = "${data.azurerm_client_config.current.tenant_id}"
+}
+
+output "azure_subscription_id" {
+    value = "${data.azurerm_client_config.current.subscription_id}"
+}
+
+output "azure_application_id" {
+    value = "${azurerm_azuread_application.vaultapp.application_id}"
+}
+
+output "azure_sp_password" {
+    value = "${azurerm_azuread_service_principal_password.vaultapp.value}"
+}
+
+output "azure_resource_group" {
+    value = "${azurerm_resource_group.vaultworkshop.name}"
+}
+
+output "Vault_Server_URL" {
+    value = "http://${azurerm_public_ip.vault-pip.fqdn}:8200"
+}
+
+output "MySQL_Server_FQDN" {
+    value = "${azurerm_mysql_server.mysql.fqdn}"
+}
+
+output "_Instructions" {
+   value = <<SHELLCOMMANDS
+
+ # Connect to your Linux Virtual Machine
+ #
+ # Run the command below to SSH into your server. You can also use PuTTY or any
+ # other SSH client. Your password is: ${var.admin_password}
+ 
+ ssh ${var.admin_username}@${azurerm_public_ip.vault-pip.fqdn}
+ SHELLCOMMANDS
+}
 
